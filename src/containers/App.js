eec59ec685699fbce86bd32c8787a73d3e2df03e
Thiago de Bastos <thiago@thiagodebastos.com>
2019-02-27 11:39:01 +1100

    chore: replace styled-components with emotion and add ThemeProvider to App Without ThemeProvider, app build will break

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import {
 } from '@atlaskit/navigation-next';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import { css, Global } from '@emotion/core';
+import { ThemeProvider } from 'styled-components';
 
 import { productHomeView } from '../config/viewConfigs';
 import { LinkItem } from '../components';
@@ -70,12 +71,15 @@ export default function() {
   const [count, setCount] = useState(0);
   return (
     <BrowserRouter>
-      <NavigationProvider>
-        <Global styles={globalStyles} />
-        <ModalLoadCtx.Provider value={[count, setCount]}>
-          <AppWithNavigationViewController />
-        </ModalLoadCtx.Provider>
-      </NavigationProvider>
+      {/* @HACK: without ThemeProvider the build will break since some components still use the theme consumer  */}
+      <ThemeProvider theme={{}}>
+        <NavigationProvider>
+          <Global styles={globalStyles} />
+          <ModalLoadCtx.Provider value={[count, setCount]}>
+            <AppWithNavigationViewController />
+          </ModalLoadCtx.Provider>
+        </NavigationProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
